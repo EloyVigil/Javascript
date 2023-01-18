ed98915d4fc57ab7e8e0021a54c2288a4cf7a153
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-01-18 09:54:09 -0800

    Add files via upload

---
+++ first-jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Land great job</li>
+          <li>Get paid the big bucks</li>
+          <li>Live the good life</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
